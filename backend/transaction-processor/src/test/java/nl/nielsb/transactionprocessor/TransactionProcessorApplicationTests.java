f762afba01d501f72a03dc36c46a6a97b038bd7b
Niels van Bruggen <nwb.van.bruggen@outlook.com>
2025-06-29 21:19:08 +0200

    Basic gradle multi-project backend setup.

---
+++ backend/transaction-processor/src/test/java/nl/nielsb/transactionprocessor/TransactionProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.nielsb.transactionprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
